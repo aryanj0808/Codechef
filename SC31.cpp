d411ab2791487c34cdb0f6bf87292d167b80987c
Aryan <aryanj0808@gmail.com>
2022-05-31 22:59:06 +0530

    SC31

--- SC31.cpp
+++ SC31.cpp
@@ -1,38 +1,42 @@
 #include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-	int t;
-	cin>>t;
-	while (t--){
-	   int n;
-	   cin>>n;
-	   int count=0;
-	   string a,b;
-	   cin>>a;
-	   //cout<<a<<endl;
-	   
-	   for(int i=0;i<n-1;i++){
-	      cin>>b;
-	   //   cout<<b<<endl;
-	       for(int j=0;j<10;j++){
-	           if(a[j]==b[j]){
-	               a[j]='0';
-	           }
-	           else{
-	               a[j]='1';
-	           }
-	       }
-	   }
-	      for(int k=0;k<10;k++){
-	          if(a[k]=='1'){
-	              count++;
-	          }
-	      }
-	    
-	   cout<<count<<endl;
-	}
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        string str;
+        cin>>str;
+        long long count=0;
+        for (int i=1;i<N;i++){
+            string s;
+            cin>>s;
+            for (int j=0;j<10;j++){
+                
+                if(str[j]==s[j]){
+                    str[j]='0';
+                }
+                else{
+                    str[j]='1';
+                }
+            }
+            
+        }
+        
+        for(int i=0;i<10;i++){
+                if(str[i]=='1'){
+                    count=count+1;
+                }
+                // cout<<str[i]<<" ";
+        }
+        cout<<count<<endl;
+        
+    }
+    
+    }
 	
-    return 0;
 
-}
